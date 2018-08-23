075e58b5869d6f73c670319fab26c34ec6c0fc44
Nate <nate@moran.ws>
2018-08-23 16:21:27 -0400

    Step 9.2

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:release web
+docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:release web --app $HEROKU_APP_NAME
